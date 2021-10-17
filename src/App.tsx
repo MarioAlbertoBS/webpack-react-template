b1fff91e55356d11cba5412b4d3edf0ed84e973f
MarioAlbertoBS <maralbo1@hotmail.com>
2021-10-17 13:19:20 -0700

    Initial Commit. Template created with basic configuration, babel, typescript and react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My React Custom Template, and i'm adding more stuff</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    // src/index.js as entry point of the app
+    entry: path.join(__dirname, "src", "index.tsx"),
+    // All the production code will be transpiled to the "dist" directory
+    output: {
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    // Defining babel configuration
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: "awesome-typescript-loader"
+                }
+            },
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }            
+            },
+        ]
+    },
+    // Html Plugin to transpile into static html and javascript
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
+}
